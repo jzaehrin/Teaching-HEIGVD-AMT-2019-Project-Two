080e89a33525d65ed345a0b882342d202f58c128
pete842 <pierrekohler94@gmail.com>
2019-12-09 11:16:13 +0100

    init auth-api

---
+++ auth-api/src/test/java/ch/heigvd/amt/bidirhandshake/authapi/AuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.heigvd.amt.bidirhandshake.authapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
