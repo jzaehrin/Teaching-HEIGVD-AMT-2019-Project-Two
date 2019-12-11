11dd7e245c690509a76bfe608e49365f5f970acf
Jonathan Zaehringer <jonathan.zaehringer@gmail.com>
2019-12-11 20:42:19 +0100

    Add PasswordAuthentifcation to check password

--- auth-api/src/main/java/ch/heigvd/amt/bidirhandshake/authapi/api/endpoints/UsersApiController.java
+++ auth-api/src/main/java/ch/heigvd/amt/bidirhandshake/authapi/api/endpoints/UsersApiController.java
@@ -1,5 +1,6 @@
 package ch.heigvd.amt.bidirhandshake.authapi.api.endpoints;
 
+import ch.heigvd.amt.bidirhandshake.authapi.api.utils.PasswordAuthentication;
 import ch.heigvd.amt.bidirhandshake.authapi.dto.UserCredential;
 import ch.heigvd.amt.bidirhandshake.authapi.entities.User;
 import ch.heigvd.amt.bidirhandshake.authapi.repositories.UserRepository;
@@ -11,15 +12,18 @@ import javax.validation.Valid;
 
 @Controller
 public class UsersApiController implements UsersApi {
+
     @Autowired
     UserRepository userRepository;
+
+    PasswordAuthentication passwordAuthentication = new PasswordAuthentication(5);
 
     public ResponseEntity<Void> login(@Valid UserCredential userCredential) {
         User user = userRepository.findByEmail(userCredential.getEmail());
 
         if (user == null) return ResponseEntity.status(404).build();
 
-        if (user.getPassword() != userCredential.getPassword()) return ResponseEntity.ok().build();
+        if (passwordAuthentication.authenticate(user.getPassword().toCharArray(), userCredential.getPassword())) return ResponseEntity.ok().build();
 
         return ResponseEntity.badRequest().build();
     }
