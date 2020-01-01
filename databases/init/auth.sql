f63d4d755102e7408ff4f3a3f33c1b8d23e07651
Jonathan Zaehringer <jonathan.zaehringer@gmail.com>
2020-01-01 19:31:42 +0100

    Add traefik structure and dockerize all project

---
+++ databases/init/auth.sql
@@ -0,0 +1,38 @@
+show databases;
+drop database if exists `authDBidir`;
+create database `authDBidir`;
+use `authDBidir`;
+
+create table if not exists `users`
+(
+    `id`           int unsigned not null auto_increment,
+    `username`     varchar(250) not null,
+    `password`     varchar(250) not null,
+    `email`        varchar(250) not null,
+    `role`         varchar(10)  not null,
+    `member_since` timestamp    not null default current_timestamp,
+
+    primary key (`id`),
+
+    constraint `email_uq` unique (`email`)
+);
+
+insert into `users`
+(`username`,
+ `password`,
+ `role`,
+ `email`)
+values ('pete842',
+        '$31$5$8f91zmgQBh7LUAM1-29me1KhHFQJekxK1872YYzNO2s',
+        'admin',
+        'pete842@mail.com'),
+       ('jzaehrin',
+        '$31$5$8f91zmgQBh7LUAM1-29me1KhHFQJekxK1872YYzNO2s',
+        'admin',
+        'jzaehrin@mail.com'),
+       ('capito27',
+        '$31$5$8f91zmgQBh7LUAM1-29me1KhHFQJekxK1872YYzNO2s',
+        'user',
+        'capito27@mail.com');
+
+select * from `users`;
---
+++ databases/init/movie.sql
@@ -0,0 +1,81 @@
+drop database if exists `movieDBidir`;
+create database `movieDBidir`;
+use `movieDBidir`;
+
+create table `medias`
+(
+    `id`         int unsigned not null auto_increment,
+    `title`      varchar(250) not null,
+    `release`    timestamp,
+    `duration`   int unsigned,
+    `main_genre` varchar(250),
+    `rating`     int unsigned,
+
+    primary key (`id`)
+);
+
+create table `users`
+(
+    `id`   int unsigned not null,
+
+    primary key (`id`)
+);
+
+create table `media_user`
+(
+    `user_id`  int unsigned not null,
+    `media_id` int unsigned not null,
+    `rating`   int unsigned,
+    `watched`  timestamp    null default null,
+
+    primary key (`user_id`, `media_id`),
+
+    foreign key (`user_id`) references `users` (`id`),
+    foreign key (`media_id`) references `medias` (`id`)
+);
+
+insert into `users`
+    (`id`)
+values (1),
+       (2),
+       (3);
+
+load data local infile '/docker-entrypoint-initdb.d/out.csv' into table `medias` fields terminated by '\t' lines terminated by '\n' (`title`, @release, `duration`, `main_genre`, `rating`) set `release` = FROM_UNIXTIME(@release);
+
+drop procedure if exists insertMediaUser;
+delimiter //
+
+create procedure insertMediaUser()
+begin
+    declare counter int default 0;
+    declare continue handler for sqlexception set counter = counter - 1;
+    begin
+        while counter < 10000
+            do
+                set counter = counter + 1;
+                insert into `media_user`
+                (`user_id`,
+                 `media_id`)
+                values (1,
+                        floor(rand() * (1000000 - 1 + 1)) + 1);
+
+                insert into `media_user`
+                (`user_id`,
+                 `media_id`,
+                 `rating`,
+                 `watched`)
+                values (1,
+                        floor(rand() * (1000000 - 1 + 1)) + 1,
+                        floor(rand() * (100 - 1 + 1)) + 1,
+                        now() - interval floor(rand() * 365) day);
+            end while;
+    end;
+end;
+//
+delimiter ;
+
+call insertMediaUser;
+
+create sequence `hibernate_sequence`
+
+SELECT * from media_user WHERE user_id = 2 AND watched is null;
